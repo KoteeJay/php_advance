cde8c8ed84993177af7ed69e2dbb2684d980c43d
dev_jay <jukotee@gmail.com>
2024-08-16 14:51:18 +0100

    Added color methods

--- index.php
+++ index.php
@@ -30,27 +30,35 @@ class Fruit{
 // Define objects for name
 $banana = new Fruit();
 $banana -> set_name("Banana");
+$banana -> set_color("The color of my banana is green");
 
 $apple = new Fruit();
 $apple -> set_name("Apple");
+$apple -> set_color("The color of my apple is red");
 
 //Define objects for color
-$yellow = new Fruit();
-$yellow -> set_color("The color of my banana is yellow");
+// $yellow = new Fruit();
+// $yellow -> set_color("The color of my banana is yellow");
 
-$green = new Fruit();
-$green -> set_color("The color of my apple is green");
+// $green = new Fruit();
+// $green -> set_color("The color of my apple is green");
 
 
 
 echo $banana -> get_name();
 echo "<br />";
+echo $banana -> get_color();
+
+echo "<br>";
+echo "<br>";
 echo $apple -> get_name();
+echo "<br>";
+echo $apple -> get_color();
 
 echo "<br />";
 echo"<br>";
 
-echo $yellow -> get_color();
-echo "<br />";
+// echo $yellow -> get_color();
+// echo "<br />";
 
-echo $green -> get_color();
+// echo $green -> get_color();
